d2a2744fcd2192a86bcefc5f6c7479f5abac862a
CAM603 <cameronhawley@comcast.net>
2020-06-08 15:20:30 -0400

    add bft

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -3,9 +3,11 @@ Simple graph implementation
 """
 from util import Stack, Queue  # These may come in handy
 
+
 class Graph:
 
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
+
     def __init__(self):
         self.vertices = {}
 
@@ -13,26 +15,47 @@ class Graph:
         """
         Add a vertex to the graph.
         """
-        pass  # TODO
+        self.vertices[vertex_id] = set()
 
     def add_edge(self, v1, v2):
         """
         Add a directed edge to the graph.
         """
-        pass  # TODO
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+        else:
+            print('Vertex does not exist')
 
     def get_neighbors(self, vertex_id):
         """
         Get all neighbors (edges) of a vertex.
         """
-        pass  # TODO
+        return self.vertices[vertex_id]
 
     def bft(self, starting_vertex):
         """
         Print each vertex in breadth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
+        # initialize queue
+        q = Queue()
+        # create set to store visited vertices
+        visited = set()
+        # add starting_vertex to queue
+        q.enqueue(starting_vertex)
+        # while queue is not empty
+        while q.size() > 0:
+            # dequeu starting_vertex
+            v = q.dequeue()
+            # if the vertex has not been visited
+            if v not in visited:
+                # visit it
+                print(v)
+                # mark as visited
+                visited.add(v)
+                # add neighbors to queue
+                for next_vert in self.get_neighbors(v):
+                    q.enqueue(next_vert)
 
     def dft(self, starting_vertex):
         """
@@ -75,6 +98,7 @@ class Graph:
         This should be done using recursion.
         """
         pass  # TODO
+
 
 if __name__ == '__main__':
     graph = Graph()  # Instantiate your graph

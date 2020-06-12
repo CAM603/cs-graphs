0e4ef3a49b6a715993eaba22668b2248307d5f32
CAM603 <cameronhawley@comcast.net>
2020-06-12 09:58:26 -0400

    first pass for adv

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -11,55 +11,55 @@ def get_neighbors(node, matrix):
     return neighbors
 
 
-# def earliest_ancestor(ancestors, starting_node):
-#     # initilize a list to represent the longest path
-#     longest_path = 1
-#     earliest_ancestor = -1
+def earliest_ancestor(ancestors, starting_node):
+    # initilize a list to represent the longest path
+    longest_path = 1
+    earliest_ancestor = -1
 
-#     # initialize stack
-#     # add starting node to stack
-#     s = Stack()
-#     s.push([starting_node])
+    # initialize stack
+    # add starting node to stack
+    s = Stack()
+    s.push([starting_node])
 
-#     # while there are paths in the stack
-#     while s.size() > 0:
+    # while there are paths in the stack
+    while s.size() > 0:
 
-#         # get the next path in line
-#         path = s.pop()
-#         # get last node in the path
-#         cur_node = path[-1]
+        # get the next path in line
+        path = s.pop()
+        # get last node in the path
+        cur_node = path[-1]
 
-#         # check to see if there is a new longest path
-#         if len(path) >= longest_path and cur_node < earliest_ancestor or len(path) > longest_path:
-#             longest_path = len(path)  # reassign longest path
-#             earliest_ancestor = cur_node
+        # check to see if there is a new longest path
+        if len(path) >= longest_path and cur_node < earliest_ancestor or len(path) > longest_path:
+            longest_path = len(path)  # reassign longest path
+            earliest_ancestor = cur_node
 
-#         for neighbor in get_neighbors(cur_node, ancestors):
-#             path_copy = list(path)
-#             path_copy.append(neighbor)
-#             s.push(path_copy)
+        for neighbor in get_neighbors(cur_node, ancestors):
+            path_copy = list(path)
+            path_copy.append(neighbor)
+            s.push(path_copy)
 
-#     return earliest_ancestor
+    return earliest_ancestor
 
 
-def earliest_ancestor(ancestors, starting_node, longest_path=1, path=None, earliest=-1):
-    if path is None:
-        path = list()
+# def earliest_ancestor(ancestors, starting_node, longest_path=1, path=None, earliest=-1):
+#     if path is None:
+#         path = list()
 
-    new_path = path + [starting_node]
+#     new_path = path + [starting_node]
 
-    if len(new_path) >= longest_path and starting_node < earliest or len(new_path) > longest_path:
-        longest_path = len(new_path)  # reassign longest path
-        earliest = starting_node
+#     if len(new_path) >= longest_path and starting_node < earliest or len(new_path) > longest_path:
+#         longest_path = len(new_path)  # reassign longest path
+#         earliest = starting_node
 
-    for neighbor in get_neighbors(starting_node, ancestors):
+#     for neighbor in get_neighbors(starting_node, ancestors):
 
-        neighbor_path = earliest_ancestor(
-            ancestors, neighbor, longest_path, new_path, earliest)
-        if neighbor_path:
-            return neighbor_path
+#         neighbor_path = earliest_ancestor(
+#             ancestors, neighbor, longest_path, new_path, earliest)
+#         if neighbor_path:
+#             return neighbor_path
 
-    return earliest
+#     return earliest
 
 
 ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7),
